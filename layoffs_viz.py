1bbc5a1345fbfec72ebc8a69c597d2e416e63590
Yashashwita Shukla <yashashwita@gmail.com>
2023-07-02 18:50:12 -0700

    Error Handling

--- layoffs_viz.py
+++ layoffs_viz.py
@@ -169,173 +169,175 @@ filter1, filter2, filter3, filter4 = st.columns(4)
 year_filter = filter1.selectbox("", ['Select Year (All)']+list(data['Year'].unique()))
 industry_filter = filter2.selectbox("", ['Select Industry (All)']+sorted(list(data['Industry'].unique())))
 country_filter = filter3.selectbox("", ['Select Country (All)']+list(data['Country'].unique()))
-company_filter = filter4.selectbox("", ['Select Company (All)']+list(data['Company'].unique()))
+company_filter = filter4.selectbox("", ['Select Company (All)']+list(data[data['# Laid Off'].notnull()]['Company'].unique()))
 
-# +
-data['dum'] = True
+try:
+    # +
+    data['dum'] = True
 
-mask = (data['dum'] == True)
+    mask = (data['dum'] == True)
 
-# Update the mask based on the selected filters
-if year_filter != 'Select Year (All)':
-    mask &= (data['Year'] == year_filter)
-if industry_filter != 'Select Industry (All)':
-    mask &= (data['Industry'] == industry_filter)
-if country_filter != 'Select Country (All)':
-    mask &= (data['Country'] == country_filter)
-if company_filter != 'Select Company (All)':
-    mask &= (data['Company'] == company_filter)
+    # Update the mask based on the selected filters
+    if year_filter != 'Select Year (All)':
+        mask &= (data['Year'] == year_filter)
+    if industry_filter != 'Select Industry (All)':
+        mask &= (data['Industry'] == industry_filter)
+    if country_filter != 'Select Country (All)':
+        mask &= (data['Country'] == country_filter)
+    if company_filter != 'Select Company (All)':
+        mask &= (data['Company'] == company_filter)
 
-# Filter the data DataFrame based on the selected filters
-filtered_data = data[mask]
+    # Filter the data DataFrame based on the selected filters
+    filtered_data = data[mask]
 
 
-# -
+    # -
 
-def top_layoffs(data,n):
-    top = data[['Day','Company','# Laid Off']].dropna().groupby(['Company']).sum().reset_index().sort_values(by=['# Laid Off'],ascending=False).dropna().head(n)
-    
-    fig = go.Figure(data=[go.Table(
-        columnwidth = [1000,600],
-        header=dict(values=[top['Company'].iloc[0], top['# Laid Off'].iloc[0]],
+    def top_layoffs(data,n):
+        top = data[['Day','Company','# Laid Off']].dropna().groupby(['Company']).sum().reset_index().sort_values(by=['# Laid Off'],ascending=False).dropna().head(n)
+        
+        fig = go.Figure(data=[go.Table(
+            columnwidth = [1000,600],
+            header=dict(values=[top['Company'].iloc[0], top['# Laid Off'].iloc[0]],
+                        line_color='#F5F1F1',
+                        fill_color='#f0f2f6',
+                        align='left',
+                    height=33.5,
+                    font_size=18),
+            cells=dict(values=[top['Company'].to_list()[1:], # 1st column
+                            top['# Laid Off'].to_list()[1:]], # 2nd column
                     line_color='#F5F1F1',
                     fill_color='#f0f2f6',
                     align='left',
-                   height=33.5,
-                   font_size=18),
-        cells=dict(values=[top['Company'].to_list()[1:], # 1st column
-                           top['# Laid Off'].to_list()[1:]], # 2nd column
-                   line_color='#F5F1F1',
-                   fill_color='#f0f2f6',
-                   align='left',
-                  height=33.5,
-                  font_size=18))
-    ])
-    
-    fig.update_layout(hovermode='closest',
-        title={
-            'text': "Top Layoffs",
-            'x': 0.5,  # Set the title's horizontal alignment to the center
-            'font': {'size': 24, 'family': 'Monospace, bold'}
-        },coloraxis_showscale=False,width=2400
+                    height=33.5,
+                    font_size=18))
+        ])
+        
+        fig.update_layout(hovermode='closest',
+            title={
+                'text': "Top Layoffs",
+                'x': 0.5,  # Set the title's horizontal alignment to the center
+                'font': {'size': 24, 'family': 'Monospace, bold'}
+            },coloraxis_showscale=False,width=2400
+        )
+        
+        st.plotly_chart(fig)
+
+
+    # +
+    t1,t2 = st.columns([4,10])
+
+    with t1:
+        top_layoffs(filtered_data,8)
+        
+    with t2:
+        time_layoff(filtered_data)
+
+
+    # -
+
+    def industry_layoff(data):
+        industry_group = data.groupby('Industry')['# Laid Off'].sum().sort_values(ascending=False).reset_index()
+
+        if len(industry_group[industry_group['Industry']!='Other'])>=8:
+            large_categories = industry_group[industry_group['Industry']!='Other'].head(8)
+            other_categories = industry_group[~industry_group['Industry'].isin(large_categories['Industry'].unique())]['# Laid Off'].sum()
+            large_categories = large_categories.append({'Industry': 'Other', '# Laid Off': other_categories}, ignore_index=True)
+        else:
+            large_categories = industry_group.copy()
+        
+        fig = px.pie(large_categories, values='# Laid Off', names='Industry',hole=0.6,
+                    color_discrete_sequence= px.colors.sequential.Reds_r)
+        
+        fig.update_layout(
+            title={
+                'text': 'Layoffs by Industry',
+                'x': 0.5,  # Set the title's horizontal alignment to the center
+                'font': {'size': 24, 'family': 'Monospace, bold'}
+            }
+        )
+        
+        # Display the chart
+        st.plotly_chart(fig)
+
+
+    def stage_layoff(data):
+        stage_group = data.groupby('Stage')['# Laid Off'].sum().reset_index()
+
+        if len(stage_group[stage_group['Stage']!='Other'])>=8:
+            large_categories = stage_group[stage_group['Stage']!='Other'].head(8)
+            other_categories = stage_group[~stage_group['Stage'].isin(large_categories['Stage'].unique())]['# Laid Off'].sum()
+            large_categories = large_categories.append({'Stage': 'Other', '# Laid Off': other_categories}, ignore_index=True)
+        else:
+            large_categories = stage_group.copy()
+        
+        fig = px.pie(large_categories, values='# Laid Off', names='Stage',hole=.6,
+                    color_discrete_sequence= px.colors.sequential.Reds_r)
+        
+        fig.update_layout(
+            title={
+                'text': 'Layoffs by Stage',
+                'x': 0.5,  # Set the title's horizontal alignment to the center
+                'font': {'size': 24, 'family': 'Monospace, bold'}
+            }, legend_traceorder="reversed"
+        )
+        
+        # Display the chart
+        st.plotly_chart(fig)
+
+
+    # +
+    plot1, plot2 = st.columns(2)
+
+    with plot1:
+        industry_layoff(filtered_data)
+    with plot2:
+        stage_layoff(filtered_data)
+
+
+    # -
+
+    def location_layoff(data):
+        
+        data['City'] = data['Location HQ'].apply(lambda x:x[0]+" ("+x[1]+")" if len(x)>1 else x[0])
+        location_group  = data.groupby('City')['# Laid Off'].sum().reset_index().sort_values('# Laid Off').tail(10)
+        
+        fig = px.bar(location_group, y="City", x="# Laid Off",orientation='h')
+        
+        fig.update_layout(
+            title={
+                'text': 'Top 10 Cities with Layoffs',
+                'x': 0.5,  # Set the title's horizontal alignment to the center
+                'font': {'size': 24, 'family': 'Monospace, bold'}
+            },
+            xaxis=dict(title=''),
+            yaxis=dict(title='')
+        )
+        
+        fig.update_traces(marker_color='rgba(254,206,186,255)')
+        
+        st.plotly_chart(fig)
+
+
+    # +
+    l1,l2 = st.columns(2)
+
+    with l1:
+        country_layoff(filtered_data,geo_json,"Layoffs by Country")
+    with l2:
+        location_layoff(filtered_data)
+    # -
+
+    st.markdown(
+        """
+        <div style='text-align: center; font-size:24px;'>
+            LayOff Reports<br>
+        </div>
+        """,
+        unsafe_allow_html=True
     )
-    
-    st.plotly_chart(fig)
 
+    st.dataframe(filtered_data[['Day','Company','Location HQ','Industry','Country','Stage','# Laid Off','Source']])
 
-# +
-t1,t2 = st.columns([4,10])
-
-with t1:
-    top_layoffs(filtered_data,8)
-    
-with t2:
-    time_layoff(filtered_data)
-
-
-# -
-
-def industry_layoff(data):
-    industry_group = data.groupby('Industry')['# Laid Off'].sum().reset_index()
-    
-    threshold = 17000
-
-    # Filter out smaller categories
-    large_categories = industry_group[industry_group['# Laid Off'] > threshold]
-    small_categories = industry_group[industry_group['# Laid Off'] <= threshold]
-    
-    large_categories.loc[large_categories[large_categories['Industry'] == 'Other'].index, '# Laid Off'] += small_categories['# Laid Off'].sum()
-    
-    fig = px.pie(large_categories, values='# Laid Off', names='Industry',hole=0.6,
-                color_discrete_sequence= px.colors.sequential.Reds)
-    
-    fig.update_layout(
-        title={
-            'text': 'Layoffs by Industry',
-            'x': 0.5,  # Set the title's horizontal alignment to the center
-            'font': {'size': 24, 'family': 'Monospace, bold'}
-        }
-    )
-    
-    # Display the chart
-    st.plotly_chart(fig)
-
-
-def stage_layoff(data):
-    stage_group = data.groupby('Stage')['# Laid Off'].sum().reset_index()
-    
-    threshold = 8000
-
-    # Filter out smaller categories
-    large_categories = stage_group[stage_group['# Laid Off'] > threshold]
-    small_categories = stage_group[stage_group['# Laid Off'] <= threshold]
-    
-    large_categories.loc[len(large_categories.index)] = ['Other', small_categories['# Laid Off'].sum() ] 
-    
-    fig = px.pie(large_categories, values='# Laid Off', names='Stage',hole=.6,
-                color_discrete_sequence= px.colors.sequential.Reds)
-    
-    fig.update_layout(
-        title={
-            'text': 'Layoffs by Stage',
-            'x': 0.5,  # Set the title's horizontal alignment to the center
-            'font': {'size': 24, 'family': 'Monospace, bold'}
-        }, legend_traceorder="reversed"
-    )
-    
-    # Display the chart
-    st.plotly_chart(fig)
-
-
-# +
-plot1, plot2 = st.columns(2)
-
-with plot1:
-    industry_layoff(filtered_data)
-with plot2:
-    stage_layoff(filtered_data)
-
-
-# -
-
-def location_layoff(data):
-    
-    data['City'] = data['Location HQ'].apply(lambda x:x[0]+" ("+x[1]+")" if len(x)>1 else x[0])
-    location_group  = data.groupby('City')['# Laid Off'].sum().reset_index().sort_values('# Laid Off').tail(10)
-    
-    fig = px.bar(location_group, y="City", x="# Laid Off",orientation='h')
-    
-    fig.update_layout(
-        title={
-            'text': 'Top 10 Cities with Layoffs',
-            'x': 0.5,  # Set the title's horizontal alignment to the center
-            'font': {'size': 24, 'family': 'Monospace, bold'}
-        },
-        xaxis=dict(title=''),
-        yaxis=dict(title='')
-    )
-    
-    fig.update_traces(marker_color='rgba(254,206,186,255)')
-    
-    st.plotly_chart(fig)
-
-
-# +
-l1,l2 = st.columns(2)
-
-with l1:
-    country_layoff(filtered_data,geo_json,"Layoffs by Country")
-with l2:
-    location_layoff(filtered_data)
-# -
-
-st.markdown(
-    """
-    <div style='text-align: center; font-size:24px;'>
-        LayOff Reports<br>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-st.dataframe(filtered_data[['Day','Company','Location HQ','Industry','Country','Stage','# Laid Off','Source']])
+except Exception as e:
+    st.error('Try another combination of filters')
